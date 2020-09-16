799bcb0a47565088e1e320376a3a7f710c79188a
Charriu <kevinkerkhoff@mailbox.org>
2020-09-16 16:15:14 +0200

    Add back things that got lost

--- CvGameCoreDLL/CvUnit.cpp
+++ CvGameCoreDLL/CvUnit.cpp
@@ -309,6 +309,8 @@ void CvUnit::reset(int iID, UnitTypes eUnit, PlayerTypes eOwner, bool bConstruct
 	m_iImmuneToFirstStrikesCount = 0;
 	m_iExtraVisibilityRange = 0;
 	m_iExtraMoves = 0;
+	//Charriu DomainAnimalCombat
+	m_iAnimalCombats = 0;
 	m_iExtraMoveDiscount = 0;
 	m_iExtraAirRange = 0;
 	m_iExtraIntercept = 0;
@@ -8756,7 +8758,7 @@ int CvUnit::cityDefenseModifier() const
 
 int CvUnit::animalCombatModifier() const
 {
-	return m_pUnitInfo->getAnimalCombatModifier();
+	return m_pUnitInfo->getAnimalCombatModifier() + GET_TEAM(getTeam()).getAnimalCombats(getDomainType());
 }
 
 
@@ -8887,6 +8889,11 @@ bool CvUnit::isFull() const
 
 int CvUnit::cargoSpaceAvailable(SpecialUnitTypes eSpecialCargo, DomainTypes eDomainCargo) const
 {
+	if (eDomainCargo == DOMAIN_SCOUT)
+	{
+		eDomainCargo = DOMAIN_LAND;
+	}
+
 	if (specialCargo() != NO_SPECIALUNIT)
 	{
 		if (specialCargo() != eSpecialCargo)
@@ -10200,6 +10207,18 @@ void CvUnit::changeExtraMoves(int iChange)
 	FAssert(getExtraMoves() >= 0);
 }
 
+//Charriu DomainAnimalCombat
+int CvUnit::getAnimalCombats() const												
+{
+	return m_iAnimalCombats;
+}
+
+
+void CvUnit::changeAnimalCombats(int iChange)			
+{
+	m_iAnimalCombats += iChange;
+	FAssert(getAnimalCombats() >= 0);
+}
 
 int CvUnit::getExtraMoveDiscount() const
 {
@@ -11518,6 +11537,8 @@ void CvUnit::read(FDataStreamBase* pStream)
 	pStream->Read(&m_iImmuneToFirstStrikesCount);
 	pStream->Read(&m_iExtraVisibilityRange);
 	pStream->Read(&m_iExtraMoves);
+	//Charriu DomainAnimalCombat
+	pStream->Read(&m_iAnimalCombats);
 	pStream->Read(&m_iExtraMoveDiscount);
 	pStream->Read(&m_iExtraAirRange);
 	pStream->Read(&m_iExtraIntercept);
@@ -11622,6 +11643,8 @@ void CvUnit::write(FDataStreamBase* pStream)
 	pStream->Write(m_iImmuneToFirstStrikesCount);
 	pStream->Write(m_iExtraVisibilityRange);
 	pStream->Write(m_iExtraMoves);
+	//Charriu DomainAnimalCombat
+	pStream->Write(m_iAnimalCombats);
 	pStream->Write(m_iExtraMoveDiscount);
 	pStream->Write(m_iExtraAirRange);
 	pStream->Write(m_iExtraIntercept);
