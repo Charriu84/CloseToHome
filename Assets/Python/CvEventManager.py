dff425f6831b2129c9d7561b9fb6aa8c79ef42e5
Charriu <kevinkerkhoff@mailbox.org>
2021-05-31 21:43:44 +0200

    Remove old Protective Tracking

--- Assets/Python/CvEventManager.py
+++ Assets/Python/CvEventManager.py
@@ -457,12 +457,12 @@ class CvEventManager:
             for iPlayer in range(gc.getMAX_PLAYERS()):
                 player = gc.getPlayer(iPlayer)
                 if (player.isAlive()):
-                    f.write("%s|||||||||||||||||||||||||||" % (player.getCivilizationDescription(1)))
+                    f.write("%s||||||||||||||||||||||||||" % (player.getCivilizationDescription(1)))
             f.write("\n")
             for iPlayer in range(gc.getMAX_PLAYERS()):
                 player = gc.getPlayer(iPlayer)
                 if (player.isAlive()):
-                    f.write("|TotalCommerce|Gold|Science|City Count|Total Pop|Inflation|Financial Bonus Lighthouse|Financial Bonus|Financial BtS Bonus|Foreign Trade Routes|Foreign Trade Income|Domestic Trade Routes|Domestic Trade Income|Protective Bonus|Domestic Protective Bonus|Domestic Better Protective Bonus|Aggressive Maintenance Bonus|City Maintenance|Unit Cost|Unit Supply|Civic Maintenance|ORG|Labor Civic|Wonders|Great Person|WW|TotalEspionage")
+                    f.write("|TotalCommerce|Gold|Science|City Count|Total Pop|Inflation|Financial Bonus Lighthouse|Financial Bonus|Financial BtS Bonus|Foreign Trade Routes|Foreign Trade Income|Domestic Trade Routes|Domestic Trade Income|Domestic Protective Bonus|Domestic Better Protective Bonus|Aggressive Maintenance Bonus|City Maintenance|Unit Cost|Unit Supply|Civic Maintenance|ORG|Labor Civic|Wonders|Great Person|WW|TotalEspionage")
             f.write("\n")
             f.close()
 
@@ -503,7 +503,7 @@ class CvEventManager:
                 player = gc.getPlayer(iPlayer)
                 team = gc.getTeam(player.getTeam())
                 if (player.isAlive()):
-                    #TotalCommerce|Gold|Science|City Count|Total Pop|Inflation|Financial Bonus Lighthouse|Financial Bonus|Financial BtS Bonus|Foreign Trade Routes|Foreign Trade Income|Domestic Trade Routes|Domestic Trade Income|Protective Bonus|Domestic Protective Bonus|Domestic Better Protective Bonus|Aggressive Maintenance Bonus|City Maintenance|Unit Cost|Unit Supply|Civic Maintenance|ORG|Labor Civic|Wonders|Great Person|WW|TotalEspionage
+                    #TotalCommerce|Gold|Science|City Count|Total Pop|Inflation|Financial Bonus Lighthouse|Financial Bonus|Financial BtS Bonus|Foreign Trade Routes|Foreign Trade Income|Domestic Trade Routes|Domestic Trade Income|Domestic Protective Bonus|Domestic Better Protective Bonus|Aggressive Maintenance Bonus|City Maintenance|Unit Cost|Unit Supply|Civic Maintenance|ORG|Labor Civic|Wonders|Great Person|WW|TotalEspionage
                     if (player.calculateBaseNetFullGoldTracking() == 0):
                         f.write("0|")
                     else:
@@ -520,7 +520,6 @@ class CvEventManager:
                     f.write("%d|" % (player.getTrackingForeignTradeRoutesCommerce()))
                     f.write("%d|" % (player.getTrackingDomesticTradeRoutes()))
                     f.write("%d|" % (player.getTrackingDomesticTradeRoutesCommerce()))
-                    f.write("%d|" % (player.getTrackingProtectiveBonus()))
                     f.write("%d|" % (player.getTrackingDomesticProtectiveBonus()))
                     f.write("%d|" % (player.getBetterTrackingDomesticProtectiveBonus()))
                     f.write("%d|" % (player.calculateUnitCostTraitReduction()))                    
