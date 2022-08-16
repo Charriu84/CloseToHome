3c5a96d571a99d40ab2c3436b377380e41381a10
Olaf Schulz <olaf_schulz@posteo.de>
2022-08-16 10:19:40 +0200

    Lift PBMod references to v10

--- Assets/Python/PitBoss/PbAdmin.py
+++ Assets/Python/PitBoss/PbAdmin.py
@@ -15,5 +15,5 @@ import string
 gc = CyGlobalContext()
 
 # Extra path for extra python modules
-pythonDir = os.path.join(gc.getAltrootDir(),'..','Python','v9')
+pythonDir = os.path.join(gc.getAltrootDir(),'..','Python','v10')
 execfile( os.path.join(pythonDir,'PbAdmin.py'))
